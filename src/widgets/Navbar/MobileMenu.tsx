7984faa6ec29880d1cac11d803332ff1f98f8bd9
Siarhei Kvachonak <siarheikvachonak@MacBook-Pro-Siarhei-2.local>
2025-06-08 14:20:17 +0200

    remove comment

--- src/widgets/Navbar/MobileMenu.tsx
+++ src/widgets/Navbar/MobileMenu.tsx
@@ -17,7 +17,6 @@ export const MobileMenu: React.FC<MobileMenuProps> = ({
         />
       )}
 
-      {/* Menu */}
       <div
         className={`fixed top-0 right-0 h-full w-80 bg-white shadow-xl z-50 transform transition-transform duration-300 ease-in-out lg:hidden ${
           isOpen ? 'translate-x-0' : 'translate-x-full'
